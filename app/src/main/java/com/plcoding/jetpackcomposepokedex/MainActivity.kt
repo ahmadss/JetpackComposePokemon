6fbf249cc991406acbf1fed5cc5ac76eb7a62079
Didiet <mobile.developer@altama.co.id>
2021-10-08 08:44:20 +0700

    -- implementasi pertama detailscreen pokemon tampilkan gambar pokemon

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/MainActivity.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/MainActivity.kt
@@ -16,9 +16,11 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import com.plcoding.jetpackcomposepokedex.pokemondetail.PokemonDetailScreen
 import com.plcoding.jetpackcomposepokedex.pokemonlist.PokemonListScreen
 import com.plcoding.jetpackcomposepokedex.ui.theme.JetpackComposePokedexTheme
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -34,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     composable("pokemon_list_screen") {
                         PokemonListScreen(navController = navController)
                     }
-                    composable("pokemon_detail_screen/{dominantColor}/{pokemontName}",
+                    composable("pokemon_detail_screen/{dominantColor}/{pokemonName}",
                         arguments = listOf(navArgument("dominantColor") {
                             type = NavType.IntType
                         },
@@ -42,17 +44,18 @@ class MainActivity : ComponentActivity() {
                                 type = NavType.StringType
                             }
                         )) {
-                        val dominantcolor = remember {
+                        val dominantColor = remember {
                             val color = it.arguments?.getInt("dominantColor")
-                            color?.let {
-                                Color(it) ?: Color.White
-                            }
-
+                            color?.let { Color(it) } ?: Color.White
                         }
-
-                        val pokemonname = remember {
+                        val pokemonName = remember {
                             it.arguments?.getString("pokemonName")
                         }
+                        PokemonDetailScreen(
+                            dominantColor = dominantColor,
+                            pokemonName = pokemonName?.toLowerCase(Locale.ROOT) ?: "",
+                            navController = navController
+                        )
                     }
                 }
             }
